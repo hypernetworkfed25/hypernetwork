660817f5f79cacaf0735a58e65b04a868ff05c39
Hifza Batool <hifzabatool990812@gmail.com>
2024-01-30 11:54:04 +0100

    Updated

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -8,27 +8,18 @@ const Search =({searchData, onSearch}) => {
     };
 
     const handleSearch = () => {
-        console.log("handleSearch function called");
-        console.log("searchTerm:", searchTerm);
-        console.log("searchData:", searchData);
+       
 
         if (Array.isArray(searchData.users)) {
           const searchTermLowerCase = searchTerm.toLowerCase().trim();
-          console.log("searchTerm:", searchTermLowerCase);
-      
           const searchResults = searchData .users.filter((users) => {
-            const lowercasedLanguages = users.languages.map((lang) => lang.toLowerCase());
-            const hasSearchTerm =
+          const lowercasedLanguages = users.languages.map((lang) => lang.toLowerCase());
+          const hasSearchTerm =
               users.firstName.toLowerCase().includes(searchTermLowerCase) ||
               users.program.toLowerCase().includes(searchTermLowerCase) ||
               lowercasedLanguages.includes(searchTermLowerCase) ||
               users.hardSkills.some((skill) => skill.skill.toLowerCase().includes(searchTermLowerCase)) ;
-          
 
-              console.log("User:", users);
-              console.log("Does user match the search term?:", hasSearchTerm);
-              console.log("Has search term?", hasSearchTerm);
-      
             return hasSearchTerm;
           });
           console.log("searchResults:", searchResults);
