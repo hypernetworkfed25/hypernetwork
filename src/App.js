fa1c902f7513c6e990f08e4688bf39aca0f14b96
Jinjing Wu <jinjing.wu@compass.com>
2024-02-01 17:12:37 +0700

    fix: rebase with delete some part and fixing post-css warning

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-import React, { useState } from "react";
 import "./App.css";
-import SearchComponent from "./Components/SearchComponent/SearchComponent";
+import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import SearchComponent from "./Components/SearchComponent/SearchComponent";
+
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
-
-  const handleSearch = (results) => {
-    setSearchResults(results);
-    setSearchResults(results);
-  };
-  // Make the search bar position fixed
-  // Check scroll distance with framer motion
-  // Z index of the hero section is above the search component
-  // Fade out the whole hero section after a certain scroll distance
-
   return (
     <ChakraProvider>
-      <div>
-        <h1>Hyper Network</h1>
-        <Search searchData={data} onSearch={handleSearch} />
-        <div>
-          {searchResults.map((result) => (
-            <div key={result.id}>
-              <p> Name: {result.firstName}</p>
-              <p>Program:{result.program}</p>
-              <p>Languages:{result.languages.join(", ")}</p>
-              <p>Hard Skills:</p>
-              <ul>
-                {result.hardSkills.map((skill, index) => (
-                  <li key={index}>
-                    {skill.skill}: {skill.comment}
-                  </li>
-                ))}
-              </ul>
-              <p>Availbility:{result.availability}</p>
-            </div>
-          ))}
-        </div>
-      </div>
       <div style={{ minHeight: "100vh" }}>
         <h1 style={{ fontSize: "96px" }}>Hyper Network</h1>
         <p>
