3ba56ab9bd97874f1cad0b67ba4e76fb9819f474
sheriff702 <ahmed.sharif@live.se>
2024-02-01 11:13:07 +0100

    w

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import StudentComponent from "./StudentComponent";
+import StudentComponent from "./Components/StudentComponent";
 
 const App = () => {
-  // if your users data is not an array, adjust accordingly
-
   return (
     <div className="App">
       <StudentComponent />
